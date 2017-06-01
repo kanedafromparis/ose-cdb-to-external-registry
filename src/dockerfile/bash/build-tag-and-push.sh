eac4068b5eb9c54a86f12e3d736338f699c3fa9e
Charles Sabourdin <kanedafromparis@gmail.com>
2017-06-01 10:43:57 +0200

    First test on custum build On branch dev Your branch is up-to-date with 'origin/dev'.
    Changes to be committed:
    	modified:   src/dockerfile/Dockerfile
    	modified:   src/dockerfile/bash/build-tag-and-push.sh

--- src/dockerfile/bash/build-tag-and-push.sh
+++ src/dockerfile/bash/build-tag-and-push.sh
@@ -9,11 +9,45 @@ if [ ! -e "${DOCKER_SOCKET}" ]; then
   exit 1
 fi
 
-if [ -n "${INPUT_IMAGE}" ]; then
-  IN_TAG="${INPUT_REGISTRY}/${INPUT_IMAGE}"
+if [ -z "${OUTPUT_REGISTRY}" ] ]; then
+  echo "OUTPUT_REGISTRY is missing"
+  exit 1
 fi
 
-if [ -n "${OUTPUT_IMAGE}" ]; then
+if [ -z "${INPUT_REGISTRY}" ] ]; then
+  echo "OUTPUT_IMAGE is missing"
+  exit 1
+fi
+
+if [ -z "${INPUT_REGISTRY}" ] ]; then
+  echo "INPUT_REGISTRY is missing"
+  exit 1
+fi
+
+#TODO improve with git describe --exact-match <commit-id>
+if [ -n "${INPUT_IMAGE}" ]; then
+  IN_TAG="${INPUT_REGISTRY}/${INPUT_IMAGE}"
+else
+  if [ -n "${IS_NAME}"]; then
+    IS_VALUE=`oc get is ${IS_NAME} -o json`
+    if [ -z "${IS_VALUE}" ]; then
+      echo "no imageStream ${IS_NAME} in this project";
+      exit 1
+    fi
+    TAG_UAT=`cat ${IS_VALUE} | jq -r ".status.tags[]|select(.tag == \"uat\")|.items|max_by(.created)|.dockerImageReference"`
+    if [ -z "${TAG_UAT}" ]; then
+      echo "no tag \"uat\" in imageStream ${IS_NAME} in this project";
+      exit 1
+    fi
+     oc tag ${IS_NAME}:uat ${IS_NAME}:production
+     IN_TAG=`cat ${IS_VALUE} | jq -r ".status.tags[]|select(.tag == \"production\")|.items|max_by(.created)|.dockerImageReference"`
+  else
+      echo "INPUT_IMAGE or IS_NAME need to be set"
+      exit 1
+  fi
+fi
+
+if [ -n "${OUTPUT_IMAGE}" ] ]; then
   OUT_TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 fi
 
